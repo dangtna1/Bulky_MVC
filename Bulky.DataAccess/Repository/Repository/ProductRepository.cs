955e7959c6c8f2360fb1ebdfb3ce08f22b12d9e3
Dawn Vu <tamquattnb@gmail.com>
2024-01-18 10:55:26 +0700

    Finish CRUD for products

---
+++ Bulky.DataAccess/Repository/Repository/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using BulkyWeb.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(x => x.Id == obj.Id); //Product or null so we use var, ok?
+            if (obj != null && objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+            
+        }
+    }
+}
--- Bulky.DataAccess/Repository/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/Repository/UnitOfWork.cs
@@ -12,11 +12,15 @@ namespace Bulky.DataAccess.Repository.Repository
     public class UnitOfWork : IUnitOfWork
     {
         public ICategoryRepository Category { get; private set; }
+
+        public IProductRepository Product { get; private set; }
+
         private readonly ApplicationDbContext _db;
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
+            Product = new ProductRepository(_db);
         }
 
         public void Save()
